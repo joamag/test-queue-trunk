667c54e8a683e0931dd94e8784fdcdd1f79abd79
João Magalhães <joamag@gmail.com>
2023-06-14 16:45:10 +0100

    chore: initial codes structure

---
+++ main.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+
+def hello():
+    print("hello world")
+
+
+def sum_two_numbers(a, b):
+    return a + b
---
+++ test.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import unittest
+
+from main import sum_two_numbers
+
+
+class TestSum(unittest.TestCase):
+    def test_sum(self):
+        self.assertEqual(sum_two_numbers(2, 3), 5)
+        self.assertEqual(sum_two_numbers(0, 0), 0)
+        self.assertEqual(sum_two_numbers(-1, 1), 0)
+
+
+if __name__ == "__main__":
+    unittest.main()
